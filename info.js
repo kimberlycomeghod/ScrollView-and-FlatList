613fb10bafb3f2cccc0d5d52ff741aff8d1c20ef
kimberlycomeghod <143330650+kimberlycomeghod@users.noreply.github.com>
2023-11-26 22:19:16 +0800

    initial change

---
+++ info.js
@@ -0,0 +1,101 @@
+export const users = [
+    {
+      id:'1',
+      name: 'KIMBERLY COMEGHOD',
+      nickName:'kim',
+      address:'Balilihan',
+      courseAndYear:'BSIT-3',
+      avatar:
+        'https://wallpapercave.com/wp/wp7810664.jpg',
+    },
+    {
+        id:'2',
+        name: 'ANALIE BARBANIDA',
+        nickName:'nal',
+        address:'Maribojoc',
+        courseAndYear:'BSOA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'3',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'4',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'5',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'6',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'7',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'8',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'9',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'10',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'11',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+  ];
